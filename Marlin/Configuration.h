b050953b7499ab8d481e88f05dbe59002828ce27
haschtl <haschtl@protonmail.com>
2020-05-28 14:20:34 +0200

    fixed compile error; build

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1136,7 +1136,7 @@ MISSING FEATURES: EMERGENCY_PARSER, SENSORLESS_HOMING, TX_BUFFER_SIZE, FAST_PWM_
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -10
+#define Z_MIN_POS -20
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 350
